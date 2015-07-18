e90e257f8d1652fa19a0710e0ce56f422c0f3462
Bailey Seymour <bailey@baileyseymour.com>
2015-07-18 10:17:16 -0400

    added PFColor to libcolorpicker.h

--- libcolorpicker.h
+++ libcolorpicker.h
@@ -11,6 +11,21 @@ UIColor *colorFromDefaultsWithKey(NSString *defaults, NSString *key, NSString *f
 }
 #endif
 
+@interface UIColor (PFColor)
++ (UIColor *)PF_colorWithHex:(NSString *)hexString;
++ (NSString *)hexFromColor:(UIColor *)color;
+@property (nonatomic, assign, readonly) CGFloat alpha;
+@property (nonatomic, assign, readonly) CGFloat red;
+@property (nonatomic, assign, readonly) CGFloat green;
+@property (nonatomic, assign, readonly) CGFloat blue;
+@property (nonatomic, assign, readonly) CGFloat hue;
+@property (nonatomic, assign, readonly) CGFloat saturation;
+@property (nonatomic, assign, readonly) CGFloat brightness;
+- (UIColor *)desaturate:(CGFloat)percent;
+- (UIColor *)lighten:(CGFloat)percent;
+- (UIColor *)darken:(CGFloat)percent;
+@end
+
 @interface PFColorAlert : NSObject
 @property (nonatomic, retain) UIWindow *popWindow;
 - (void)showWithStartColor:(UIColor *)startColor showAlpha:(BOOL)showAlpha completion:(void (^)(UIColor *pickedColor))completionBlock;
